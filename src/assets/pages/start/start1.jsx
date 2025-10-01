e29a9bcbf0b657e9ec34a416ce8a84f8033834f2
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2025-10-01 07:20:21 +0700

    fat: update navigate4

--- src/assets/pages/start/start1.jsx
+++ src/assets/pages/start/start1.jsx
@@ -1,22 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 
 const Start1 = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-[#F0E7D5] h-screen w-full flex justify-center items-center">
       <div className="flex flex-col justify-center items-center gap-5 bg-[#F0E7D5] w-[630px] h-[453px] rounded-2xl shadow-2xl border-2 border-gray-500/20">
         <h1 className="text-5xl font-bold mb-5 cursor-pointer">TicTacToe</h1>
-        <a
-          className="bg-transparant text-black px-14 py-2 border border-black rounded-lg hover:bg-black hover:text-white transition-all duration-700 cursor-pointer"
-          href="/start"
+
+        <Button
+          className="px-14 py-2 border border-black rounded-lg hover:bg-black hover:text-white transition-all duration-700 cursor-pointer"
+          type="button"
+          onClick={() => navigate("/start")}
         >
           Start Game
-        </a>
-        <a
-          className="bg-transparant text-black px-15 py-2 border border-black rounded-lg hover:bg-red-600 hover:text-white transition-all duration-700 cursor-pointer"
-          href="google.com"
+        </Button>
+
+        <Button
+          className="px-[15px] py-2 border border-black rounded-lg hover:bg-red-600 hover:text-white transition-all duration-700 cursor-pointer"
+          type="button"
+          onClick={() => (window.location.href = "https://google.com")}
         >
           Quit Game
-        </a>
+        </Button>
       </div>
     </div>
   );
--- src/assets/pages/start/start2.jsx
+++ src/assets/pages/start/start2.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 
 const Start2 = () => {
+  const navigate = useNavigate();
   return (
     <div className="bg-[#F0E7D5] h-screen w-full flex justify-center items-center">
       <div className="flex flex-col justify-center items-center bg-[#F0E7D5] w-[630px] h-[453px] rounded-2xl shadow-2xl border-2 border-gray-500/20">
@@ -17,18 +19,24 @@ const Start2 = () => {
           </span>
         </div>
         <div className="flex justify-between items-center w-full px-10">
-          <a
-            className="bg-transparant text-black px-14 py-2 border border-black rounded-lg hover:bg-black hover:text-white transition-all duration-700 cursor-pointer"
-            hraf="/"
+          <Button
+            color="bg-transparant"
+            textColor="text-black"
+            className="px-14 py-2 border border-black rounded-lg hover:bg-black hover:text-white transition-all duration-700 cursor-pointer"
+            type="button"
+            onClick={() => navigate("/")}
           >
             Back
-          </a>
-          <a
-            className="bg-transparant text-black px-14 py-2 border border-black rounded-lg hover:bg-black hover:text-white transition-all duration-700 cursor-pointer"
-            href="/login"
+          </Button>
+          <Button
+            color="bg-transparant"
+            textColor="text-black"
+            className="px-14 py-2 border border-black rounded-lg hover:bg-black hover:text-white transition-all duration-700 cursor-pointer"
+            type="button"
+            onClick={() => navigate("/login")}
           >
             Next
-          </a>
+          </Button>
         </div>
       </div>
     </div>
